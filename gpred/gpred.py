0d8eafead7fc390bb79bbce9b843120240a25eac
Ale6 <alexbel28@yahoo.fr>
2022-10-20 18:01:17 +0200

    final

--- gpred/gpred.py
+++ gpred/gpred.py
@@ -41,16 +41,16 @@ def get_arguments():
     parser = argparse.ArgumentParser(description=__doc__, usage=
                                      "{0} -h"
                                      .format(sys.argv[0]))
-    parser.add_argument('-i', dest='genome_file', type=isfile, required=True, 
+    parser.add_argument('-i', dest='genome_file', type=isfile, required=True,
                         help="Complete genome file in fasta format")
-    parser.add_argument('-g', dest='min_gene_len', type=int, 
+    parser.add_argument('-g', dest='min_gene_len', type=int,
                         default=50, help="Minimum gene length to consider")
-    parser.add_argument('-s', dest='max_shine_dalgarno_distance', type=int, 
+    parser.add_argument('-s', dest='max_shine_dalgarno_distance', type=int,
                         default=16, help="Maximum distance from start codon "
                         "where to look for a Shine-Dalgarno motif")
     parser.add_argument('-d', dest='min_gap', type=int, default=40,
                         help="Minimum gap between two genes (shine box not included).")
-    parser.add_argument('-p', dest='predicted_genes_file', type=str, 
+    parser.add_argument('-p', dest='predicted_genes_file', type=str,
                         default=os.curdir + os.sep +"predict_genes.csv",
                         help="Tabular file giving position of predicted genes")
     parser.add_argument('-o', dest='fasta_file', type=str,
@@ -66,7 +66,7 @@ def read_fasta(fasta_file):
         ephem = ""
         for line in monfich:
             if line.startswith(">"):
-                    continue
+                continue
             ephem += line
             ephem = ephem.strip()
         return ephem.upper()
@@ -92,21 +92,42 @@ def find_stop(stop_regex, sequence, start):
 def has_shine_dalgarno(shine_regex, sequence, start, max_shine_dalgarno_distance):
     """Find a shine dalgarno motif before the start codon
     """
-    debut = start - max_shine_dalgarno_distance 
-    fin = start - 6
-    if debut <= 0:
-        debut = 0
+    fin = start - max_shine_dalgarno_distance
+    debut = start - 6
     soluce = shine_regex.search(sequence)
-    if soluce != None:
-        if soluce.start(0) < debut and soluce.end(0) < fin:
+
+    if soluce is not None:
+        if debut >= soluce.start(0)  + 1 >= fin and \
+            debut >= soluce.end(0) + 1 >= fin:
             return True
+
     return False
 
-def predict_genes(sequence, start_regex, stop_regex, shine_regex, 
+def predict_genes(sequence, start_regex, stop_regex, shine_regex,
                   min_gene_len, max_shine_dalgarno_distance, min_gap):
     """Predict most probable genes
     """
-    pass
+    pos = 0
+    mem = []
+    while len(sequence) - pos >= min_gap:
+        pos = find_start(start_regex, sequence, pos, len(sequence))
+        if pos is not None:
+            stop = find_stop(stop_regex, sequence, pos)
+            if stop is not None:
+                taille = stop - pos
+                if taille >= min_gene_len:
+                    if has_shine_dalgarno(shine_regex, sequence, pos, max_shine_dalgarno_distance):
+                        mem.append([pos + 1, stop + 3])
+                        pos = pos + stop + 2 + min_gap
+                    else:
+                        pos += 1
+                else:
+                    pos += 1
+            else:
+                pos += 1
+        else:
+            pos += 1
+    return mem
 
 
 def write_genes_pos(predicted_genes_file, probable_genes):
@@ -131,14 +152,14 @@ def write_genes(fasta_file, sequence, probable_genes, sequence_rc, probable_gene
     """
     try:
         with open(fasta_file, "wt") as fasta:
-            for i,gene_pos in enumerate(probable_genes):
+            for index, gene_pos in enumerate(probable_genes):
                 fasta.write(">gene_{0}{1}{2}{1}".format(
-                    i+1, os.linesep, 
+                    index + 1, os.linesep,
                     fill(sequence[gene_pos[0]-1:gene_pos[1]])))
-            i = i+1
-            for j,gene_pos in enumerate(probable_genes_comp):
+                index = index+1
+            for j, gene_pos in enumerate(probable_genes_comp):
                 fasta.write(">gene_{0}{1}{2}{1}".format(
-                            i+1+j, os.linesep,
+                            index + 1 + j, os.linesep,
                             fill(sequence_rc[gene_pos[0]-1:gene_pos[1]])))
     except IOError:
         sys.exit("Error cannot open {}".format(fasta_file))
@@ -157,28 +178,20 @@ def main():
     """
     Main program function
     """
-    # Gene detection over genome involves to consider a thymine instead of
-    # an uracile that we would find on the expressed RNA
-    #start_codons = ['TTG', 'CTG', 'ATT', 'ATG', 'GTG']
-    #stop_codons = ['TAA', 'TAG', 'TGA']
+
     start_regex = re.compile('AT[TG]|[ATCG]TG')
     stop_regex = re.compile('TA[GA]|TGA')
-    # Shine AGGAGGUAA
-    #AGGA ou GGAGG 
+
     shine_regex = re.compile('A?G?GAGG|GGAG|GG.{1}GG')
-    # Arguments
     args = get_arguments()
-    # Let us do magic in 5' to 3'
-    
-    # Don't forget to uncomment !!!
-    # Call these function in the order that you want
-    # We reverse and complement
-    #sequence_rc = reverse_complement(sequence)
-    # Call to output functions
-    #write_genes_pos(args.predicted_genes_file, probable_genes)
-    #write_genes(args.fasta_file, sequence, probable_genes, sequence_rc, probable_genes_comp)
-
-
+    seq_a = read_fasta(args.genome_file)
+    result_a = predict_genes(seq_a, start_regex, stop_regex, shine_regex,
+                args.min_gene_len, args.max_shine_dalgarno_distance, args.min_gap)
+    seq_b = reverse_complement(seq_a)
+    result_b = predict_genes(seq_b, start_regex, stop_regex, shine_regex,
+                args.min_gene_len, args.max_shine_dalgarno_distance, args.min_gap)
+    write_genes_pos("predict.csv", result_a)
+    write_genes("predica.csv", seq_a, result_a, seq_b, result_b)
 
 if __name__ == '__main__':
     main()
